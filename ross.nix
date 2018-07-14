b3e1257f1006d1c71c9d1b493fc70cc90e06e7de
Ross A. Baker <ross@rossabaker.com>
2018-07-14 13:32:33 -0400

    Configure git

--- ross.nix
+++ ross.nix
@@ -16,4 +16,14 @@
 
   # Broken, I think due to https://github.com/NixOS/nixos-channel-scripts/issues/9
   programs.command-not-found.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Ross A. Baker";
+    userEmail = "ross@rossabaker.com";
+    aliases = {
+      "st" = "status --short";
+    };
+    ignores = [ "*~" "\#*#" "*.elc" ".\#*" ];
+  };
 }
