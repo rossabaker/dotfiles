c9b7ad00c6bf30a4f61dd01ce6e837b885ff8751
Ross A. Baker <ross@rossabaker.com>
2018-07-15 00:00:35 -0400

    xmonad spacing

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,10 @@
 import XMonad
 import XMonad.Config.Mate
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 
 main = xmonad mateConfig
   { layoutHook = myLayout
   }
 
-myLayout = avoidStruts $ layoutHook mateConfig
+myLayout = spacingWithEdge 6 $ avoidStruts $ layoutHook mateConfig
