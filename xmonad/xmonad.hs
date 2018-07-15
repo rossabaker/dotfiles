c4bf5afe379bc40db4f85841fcce244cdba41b8c
Ross A. Baker <ross@rossabaker.com>
2018-07-15 10:24:28 -0400

    Top border on windows

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,10 +1,27 @@
 import XMonad
 import XMonad.Config.Mate
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.Spacing
 
 main = xmonad mateConfig
-  { layoutHook = myLayout
+  { borderWidth = 0                -- handled in myLayout with addTopBar
+  , layoutHook = myLayout
   }
 
-myLayout = spacingWithEdge 6 $ avoidStruts $ layoutHook mateConfig
+myBorderWidth   = 6
+myActiveColor   = "#cfb53b"
+myInactiveColor = "#808080"
+
+myLayout = addTopBar $ smartSpacingWithEdge (fromIntegral myBorderWidth) $ avoidStruts $ layoutHook mateConfig
+  where
+    addTopBar = noFrillsDeco shrinkText topBarTheme
+    topBarTheme = def
+      { decoHeight          = myBorderWidth
+      , inactiveColor       = myInactiveColor
+      , inactiveBorderColor = myInactiveColor
+      , inactiveTextColor   = myInactiveColor
+      , activeColor         = myActiveColor
+      , activeTextColor     = myActiveColor
+      , activeBorderColor   = myActiveColor
+      }
