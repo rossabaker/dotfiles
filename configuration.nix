997a0b4720cc78c5dcc39310bfb39ae931ea6313
Ross A. Baker <ross@rossabaker.com>
2018-07-17 09:21:24 -0400

    Make it run on herbert. TODO: unbreak homer.

--- configuration.nix
+++ configuration.nix
@@ -15,15 +15,19 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.version = 2;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "homer"; # Define your hostname.
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "herbert"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   # Select internationalisation properties.
