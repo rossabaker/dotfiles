b4965a41d51345bf883157df132c25050bc1e66a
Ross A. Baker <ross@rossabaker.com>
2018-11-24 14:02:07 -0500

    xmobar

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,24 @@
 import XMonad
- 
-main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
-        }
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ def
+    { manageHook = manageDocks <+> manageHook def
+    , layoutHook = avoidStruts  $  layoutHook def
+    , logHook = dynamicLogWithPP xmobarPP
+                { ppCurrent = xmobarColor "white" "#4285F4"
+                , ppVisible = xmobarColor "white" "#4285F4"
+                , ppHidden = xmobarColor "white" "#303030"
+                , ppHiddenNoWindows = xmobarColor "gray50" "#282828"
+                , ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "white" "" . shorten 40
+                }
+    , modMask = mod4Mask     -- Rebind Mod to the Windows key
+    } `additionalKeys`
+        [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
+        ]
