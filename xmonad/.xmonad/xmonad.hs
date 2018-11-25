95cb1238c9fb773657c99eb4195a27f84415b955
Ross A. Baker <ross@rossabaker.com>
2018-11-25 01:15:57 -0500

    s-r renames the workspace

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Actions.WorkspaceNames
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
@@ -7,28 +8,29 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
+import XMonad.Prompt
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run
-import System.IO
 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ ewmh def
     { manageHook = manageDocks <+> manageHook def
     , layoutHook = myLayout
-    , logHook = dynamicLogWithPP xmobarPP
+    , logHook = workspaceNamesPP xmobarPP
                 { ppCurrent = xmobarColor "white" "#4285F4"
                 , ppVisible = xmobarColor "white" "#4285F4"
                 , ppHidden = xmobarColor "white" "#303030"
                 , ppHiddenNoWindows = xmobarColor "gray50" "#282828"
                 , ppOutput = hPutStrLn xmproc
                 , ppTitle = xmobarColor "white" "" . shorten 40
-                }
+                } >>= dynamicLogWithPP
     , modMask = mod4Mask     -- Rebind Mod to the Windows key
     } `additionalKeys`
     [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
-    , ((mod4Mask, xK_p), rofi "run")
     , ((mod4Mask, xK_o), rofi "window")
+    , ((mod4Mask, xK_p), rofi "run")
+    , ((mod4Mask, xK_r), renameWorkspace prompt)
     ]
 
 myLayout = avoidStruts $ tall ||| wide ||| full
@@ -60,3 +62,10 @@ myLayout = avoidStruts $ tall ||| wide ||| full
       }
 
 rofi cmd = safeSpawn "rofi" [ "-show", cmd ]
+
+prompt :: XPConfig
+prompt = def
+  { font = "xft:DejaVu Sans Mono:size=16"
+  , height = 28
+  , position = Top
+  }
