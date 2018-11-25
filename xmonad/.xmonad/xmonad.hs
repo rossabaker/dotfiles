741400b5f06096eb0562281ace2eac9a384a4f0a
Ross A. Baker <ross@rossabaker.com>
2018-11-24 23:53:56 -0500

    topBarTheme for xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,11 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Decoration
+import XMonad.Layout.NoBorders
+import XMonad.Layout.NoFrillsDecoration
+import XMonad.Layout.Renamed
+import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 import System.IO
@@ -9,7 +14,7 @@ main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ def
     { manageHook = manageDocks <+> manageHook def
-    , layoutHook = avoidStruts  $  layoutHook def
+    , layoutHook = myLayout
     , logHook = dynamicLogWithPP xmobarPP
                 { ppCurrent = xmobarColor "white" "#4285F4"
                 , ppVisible = xmobarColor "white" "#4285F4"
@@ -20,5 +25,33 @@ main = do
                 }
     , modMask = mod4Mask     -- Rebind Mod to the Windows key
     } `additionalKeys`
-        [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
-        ]
+    [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
+    ]
+
+myLayout = avoidStruts $ tall ||| wide ||| full
+  where
+    gap = (fromIntegral 6)
+    tall = renamed [Replace "tall"] $
+      addTopBar $
+      noBorders $
+      smartSpacingWithEdge gap $
+      Tall 1 0.025 0.5
+    wide = renamed [Replace "wide"] $
+      addTopBar $
+      noBorders $
+      smartSpacingWithEdge gap $
+      Mirror $
+      Tall 1 0.025 0.5
+    full = renamed [Replace "full"] $ noBorders $ Full
+    addTopBar = noFrillsDeco shrinkText topBarTheme
+    topBarTheme = def 
+      { inactiveBorderColor   = "gray40"
+      , inactiveColor         = "gray40"
+      , inactiveTextColor     = "gray40"
+      , activeBorderColor     = "#4285F4"
+      , activeColor           = "#4285F4"
+      , activeTextColor       = "#4285F4"
+      , urgentBorderColor     = "#EA4335"
+      , urgentTextColor       = "#EA4335"
+      , decoHeight            = 10
+      }
