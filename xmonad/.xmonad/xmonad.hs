8d0d8ddfec746a818fef50802a50e96b79de949a
Ross A. Baker <ross@rossabaker.com>
2018-11-28 00:05:05 -0500

    9 workspaces ought to be enough for anybody

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -35,7 +35,7 @@ main = do
 
 myModMask = mod4Mask -- Rebind Mod to the Windows key
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
 myKeys =
   [ ((myModMask, xK_b), sendMessage ToggleStruts)
