3c33ab06e1db6ce4cb7c40ff8cb48f9b74dd41a0
Ross A. Baker <ross@rossabaker.com>
2018-12-13 20:23:22 -0500

    ivy/counsel/swiper: general bindings coming soon

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,6 +24,23 @@
   :config
   (load-theme 'base16-material t nil))
 
+(use-package counsel
+  :ensure t
+  :config
+  ;; (global-set-key (kbd "M-x") 'counsel-M-x)
+  ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  ;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  ;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  ;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  ;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  ;; (global-set-key (kbd "C-c g") 'counsel-git)
+  ;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  ;; (global-set-key (kbd "C-c k") 'counsel-ag)
+  ;; (global-set-key (kbd "C-x l") 'counsel-locate)
+  ;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  )
+
 (use-package cus-edit
   :config
   ;; Banish customizations to a gitignored file.
@@ -42,6 +59,14 @@
   :ensure t
   :hook (haskell-mode))
 
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  ;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  )
+
 (use-package menu-bar
   :config
   (menu-bar-mode -1))
@@ -49,6 +74,11 @@
 (use-package scroll-bar
   :config
   (scroll-bar-mode -1))
+
+(use-package swiper
+  :config
+  ;; (global-set-key (kbd "C-s") 'swiper)
+  )
 
 (use-package tool-bar
   :config
