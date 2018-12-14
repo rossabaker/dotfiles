c6223d6d0b4e401626f4bc2f738304ab800fbadb
Ross A. Baker <ross@rossabaker.com>
2018-12-13 19:48:03 -0500

    Minimize the UI

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,3 +10,21 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
+
+;; Packages
+
+(use-package menu-bar
+  :config
+  (menu-bar-mode -1))
+
+(use-package scroll-bar
+  :config
+  (scroll-bar-mode -1))
+
+(use-package tool-bar
+  :config
+  (tool-bar-mode -1))
+
+(use-package tooltip
+  :config
+  (tooltip-mode -1))
