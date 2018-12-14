b384ea54b5d09f93326dcdb52c21a88a33955e5c
Ross A. Baker <ross@rossabaker.com>
2018-12-14 16:06:37 -0500

    Flyspell

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -115,6 +115,20 @@
     "fei" 'ross/find-user-init-file
     "fer" 'ross/reload-user-init-file))
 
+(use-package flyspell
+  :ensure t
+  :config
+  (add-hook 'text-mode-hook #'flyspell-mode)
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+
+(use-package flyspell-correct-ivy
+  :ensure t
+  :config
+  (setq flyspell-correct-interface #'flyspell-correct-ivy)
+  :general
+  (:keymaps 'flyspell-mode-map
+            "C-;" 'flyspell-correct-wrapper))
+
 (use-package frame
   :config
   (set-face-attribute 'default nil
