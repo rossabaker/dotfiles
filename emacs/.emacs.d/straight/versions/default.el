7175d0dc24cfaf3c22a2c92d8b29dd6944f7bdbe
Ross A. Baker <ross@rossabaker.com>
2019-04-17 10:45:16 -0400

    Rebuild emacs config on straight.el

---
+++ emacs/.emacs.d/straight/versions/default.el
@@ -0,0 +1,8 @@
+(("cl-lib" . "deb92588d9d83d46c4698936ea31c5f58d244f04")
+ ("epkgs" . "6ad16995cc950f7a4e8a4b946001c40c72754ffa")
+ ("gnu-elpa-mirror" . "e979b4211660a2cc4377ef04918979fe9b4323ed")
+ ("melpa" . "ec27ae185c0308c445e461dc84f398483ca08c5a")
+ ("no-littering" . "99ae007ead688689b5b25a9482f98ec67663bb61")
+ ("straight.el" . "46aa1b1ee10bfd3a20a419c3f70d7a9c6d369fb4")
+ ("use-package" . "0ee88b5cac41acf974c4bedb8859931ead8bfc79"))
+:saturn
