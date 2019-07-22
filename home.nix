71af22ba0bd3cf26b29fe79be7d788495deda8be
Ross A. Baker <ross@rossabaker.com>
2019-07-21 23:16:12 -0400

    Configure git

--- home.nix
+++ home.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  programs.git = {
+    enable = true;
+    userEmail = "ross@rossabaker.com";
+    userName = "Ross A. Baker";
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
