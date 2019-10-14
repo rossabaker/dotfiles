31d146111c0ab2faadd30c03d6d3eee4eb651321
Ross A. Baker <ross@rossabaker.com>
2019-10-14 10:06:38 -0400

    autorandr

--- home.nix
+++ home.nix
@@ -30,6 +30,28 @@ in
   };
 
   programs = {
+    autorandr = {
+      enable = true;
+
+      profiles = {
+        "mobile" = {
+          fingerprint = {
+            eDP-1-1 = "00ffffffffffff0006afeb3200000000251b0104a5221378020925a5564f9b270c50540000000101010101010101010101010101010152d000a0f0703e803020350058c1100000180000000f0000000000000000000000000020000000fe0041554f0a202020202020202020000000fe00423135365a414e30332e32200a000d";
+          };
+          config = {
+            eDP-1-1 = {
+              enable = true;
+              dpi = 160;
+              mode = "3840x2160";
+              position = "0x0";
+              primary = true;
+              rate = "60.00";
+            };
+          };
+        };
+      };
+    };
+
     bash = {
       enable = true;
       initExtra = ''
