0595fdfd125c780c8b5926600f5af5f53853b347
Ross A. Baker <ross@rossabaker.com>
2019-11-14 20:32:16 -0500

    Extract common NixOS config

--- hosts/homer/configuration.nix
+++ hosts/homer/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../nixos.nix
     ];
 
   boot.earlyVconsoleSetup = true;
@@ -25,7 +26,6 @@
   ];
 
   networking.hostName = "homer"; # Define your hostname.
-  networking.networkmanager.enable = true;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -34,78 +34,9 @@
   networking.interfaces.enp0s31f6.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n = {
     consoleFont = "ter-132b";
     consolePackages = with pkgs; [ terminus_font ];
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Indiana/Indianapolis";
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  services.upower.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager = {
-    default = "xterm";
-    xterm.enable = true;
-  };
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ross = {
-    isNormalUser = true;
-    extraGroups = [
-      "audio"
-      "wheel"
-    ];
   };
 
   # This value determines the NixOS release with which your system is to be
@@ -113,13 +44,4 @@
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.09"; # Did you read the comment?
-
-  nix.nixPath = [
-    "nixpkgs=${../../nixpkgs}"
-    "nixos=${../../nixpkgs}"
-  ];
-
-  nix.trustedUsers = [ "root" "ross" ];
-
-  nixpkgs.config.allowUnfree = true;
 }
---
+++ nixos.nix
@@ -0,0 +1,50 @@
+{
+  nix = {
+    nixPath = [
+      "nixpkgs=${./nixpkgs}"
+      "nixos=${./nixpkgs}"
+    ];
+    trustedUsers = [ "root" "ross" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  i18n = {
+    consoleKeyMap = ./config/kbd/keys.map;
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "America/Indiana/Indianapolis";
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  services.upower.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.libinput.enable = true;
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager = {
+    default = "xterm";
+    xterm.enable = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ross = {
+    isNormalUser = true;
+    extraGroups = [
+      "audio"
+      "wheel"
+    ];
+  };
+}
