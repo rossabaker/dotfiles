49b2f3d737efb16cc7c3b2f25e5f274dfd897573
Ross A. Baker <ross@rossabaker.com>
2019-11-21 18:08:46 -0500

    gpg sign git commits

--- hosts/herbert/home.nix
+++ hosts/herbert/home.nix
@@ -5,4 +5,9 @@
     (import ../../config/home-linux.nix { dpi = 90; pkgs = pkgs; })
     (import ../../modules/work.nix)
   ];
+
+  programs.git.signing = {
+    key = "0x0CACC3F315F745F1";
+    signByDefault = true;
+  };
 }
