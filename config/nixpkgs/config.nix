2e9d3c7233868eeaa27838feb6a96ecf08bc43ba
Ross A. Baker <ross@rossabaker.com>
2019-12-11 20:55:29 -0500

    Add emacs-overlay

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -2,6 +2,7 @@
   allowUnfree = true;
 
   overlays = [
+    (import ../../emacs-overlay)
     (import ../../overlays/materia-tomorrow-theme)
     (import ../../overlays/qogir-tomorrow-theme)
     (import ../../overlays/zoom-us.nix)
