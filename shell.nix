b3c9e071b8d10b935cdbd308e6dee4c3a8ffb35b
Ross A. Baker <ross@rossabaker.com>
2019-12-19 17:05:18 -0500

    Add a nix shell for bootstrapping

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ sources ? import ./nix/sources.nix }:
+
+let
+  overlay = _: pkgs: {
+    niv = import sources.niv {};
+  };
+  pkgs = import sources.nixpkgs {
+    overlays = [ overlay ];
+    config = {};
+  };
+in
+  with pkgs; pkgs.mkShell {
+    buildInputs = [
+      gnumake
+      jq
+      niv.niv
+    ];
+  }
