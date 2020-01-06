3a5d0d376edd4590aa76c89d194354131564baf3
Ross A. Baker <ross@rossabaker.com>
2020-01-06 09:32:06 -0500

    Disable scroll-margin in command- and tail-like modes

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -32,6 +32,14 @@
               indent-tabs-mode nil)
 (put 'narrow-to-region 'disabled nil)
 
+;; scroll-margin is irritating in modes where the focus tends to be the bottom
+(defun ross/unset-scroll-margin ()
+  "Set \"scroll-margin\" to zero."
+  (setq-local scroll-margin 0))
+(dolist (hook '(messages-buffer-mode-hook comint-mode-hook term-mode-hook))
+  (remove-hook hook 'ross/unset-scroll-margin)
+  (add-hook hook 'ross/unset-scroll-margin))
+
 ;; We shall endeavor to keep everything out of this, but sometimes
 ;; Emacs really wants to dump custom settings itself.
 (setq custom-file (concat user-emacs-directory "custom.el"))
