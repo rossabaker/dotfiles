b9d6ef25a4955f390228f768f8de59c952c277d1
Ross A. Baker <ross@rossabaker.com>
2020-03-26 22:13:34 -0400

    Unpin Zoom

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -8,6 +8,5 @@ in
     (import sources.emacs-overlay)
     (import ../../overlays/materia-tomorrow-theme)
     (import ../../overlays/qogir-tomorrow-theme)
-    (import ../../overlays/zoom-us.nix)
   ];
 }
