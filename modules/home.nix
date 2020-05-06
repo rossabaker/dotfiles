17a8283d1a76091e31d02fd10349953dc06e8a47
Ross A. Baker <ross@rossabaker.com>
2020-05-06 16:42:53 -0400

    dunst

--- modules/home.nix
+++ modules/home.nix
@@ -124,6 +124,54 @@ rec {
   };
 
   services = {
+    dunst = {
+      enable = true;
+      settings = {
+        global = {
+          dmenu = "${pkgs.rofi}/bin/rofi -dmenu";
+          follow = "mouse";
+          format = "<b>%s</b>\\n%b";
+          frame_color = "#555555";
+          frame_width = 2;
+          geometry = "500x5-5+30";
+          horizontal_padding = 8;
+          icon_position = "off";
+          line_height = 0;
+          markup = "full";
+          padding = 8;
+          separator_color = "frame";
+          separator_height = 2;
+          transparency = 10;
+          word_wrap = true;
+        };
+
+        shortcuts = {
+          context = "mod4+grave";
+          close = "mod4+backslash";
+        };
+
+        urgency_low = {
+          background = "#1d1f21";
+          foreground = "#4da1af";
+          frame_color = "#4da1af";
+          timeout = 10;
+        };
+
+        urgency_normal = {
+          background = "#1d1f21";
+          foreground = "#70a040";
+          frame_color = "#70a040";
+          timeout = 15;
+        };
+
+        urgency_critical = {
+          background = "#1d1f21";
+          foreground = "#dd5633";
+          frame_color = "#dd5633";
+          timeout = 0;
+        };
+      };
+    };
 
     emacs.enable = true;
 
