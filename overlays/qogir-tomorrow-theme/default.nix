45346c3922e8d677d6ffa5c94ded820a56defd3e
Ross A. Baker <ross@rossabaker.com>
2020-05-20 16:41:35 -0400

    Upgrade, with pinned sassc

--- overlays/qogir-tomorrow-theme/default.nix
+++ overlays/qogir-tomorrow-theme/default.nix
@@ -1,10 +1,18 @@
-self: super: {
+self: super: let
+  pin = self.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "8686922e68dfce2786722acad9593ad392297188";
+    sha256 = "1pc92s1nbr9hlsmzlf8w2pc90rlma649y3fvyfww0sbcwn0lb65n";
+  };
+  pinnedPkgs = import pin {};
+in {
   qogir-tomorrow-theme = super.qogir-theme.overrideAttrs (
     old: rec {
       pname = "qogir-tomorrow-theme";
 
       buildInputs = old.buildInputs ++ [
-        self.sassc
+        pinnedPkgs.sassc # infinite loop starting after 3.6.1
         self.which # to find sassc
       ];
 
