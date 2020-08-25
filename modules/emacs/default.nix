87ac7e6c5ec8d6396ecb05fafe02f3a888e620e8
Ross A. Baker <ross@rossabaker.com>
2020-08-25 15:27:11 -0400

    Okay, let's try Doom again.

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -1,81 +1,31 @@
 { pkgs, ... }:
 
 let
-  used-packages = import (
-    pkgs.runCommand "used-packages" rec {
-      emacs = pkgs.emacsWithPackages (epkgs: [ epkgs.use-package ]);
-      buildInputs = [ emacs ];
-      srcs = [ ./used-packages.el ./init.el ];
-    } ''
-      mkdir -p $out
-      emacs --batch -l ${./used-packages.el} --eval '(ross/used-packages "${./init.el}")' > $out/default.nix
-    ''
-  );
-
-  # Things we want to ensure are on the exec-path.
-  extraSystemPackages = with pkgs; [
-    fd
-    git
-    ripgrep
-  ];
-in
-{
-  home.file = {
-    ".emacs.d/custom.el".source = ./custom.el;
-    ".emacs.d/init.el".source = ./init.el;
+  sources = import ../../nix/sources.nix;
+  doom-emacs = pkgs.callPackage sources.nix-doom-emacs {
+    doomPrivateDir = ./doom.d;
   };
-
-  home.packages = [ pkgs.emacs-all-the-icons-fonts ];
+in {
+  home.packages = [
+    pkgs.aspell
+    pkgs.aspellDicts.en
+    pkgs.aspellDicts.en-computers
+    pkgs.direnv
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.docker-machine
+    pkgs.emacs-all-the-icons-fonts
+    pkgs.git
+    pkgs.fd
+    pkgs.languagetool
+    pkgs.mdl
+    pkgs.pandoc
+    pkgs.ripgrep
+    pkgs.wordnet
+  ];
 
   programs.emacs = {
     enable = true;
-    package = pkgs.emacsUnstable;
-    extraPackages = epkgs: (used-packages epkgs) ++ extraSystemPackages;
-
-    overrides = self: super:
-      let
-        inherit (pkgs) fetchFromGitHub fetchurl stdenv;
-        inherit (stdenv) lib;
-
-        withPatches = pkg: patches:
-          lib.overrideDerivation pkg (attrs: { inherit patches; });
-      in
-        {
-          quick-yes =
-            let
-              version = "10";
-            in
-              stdenv.mkDerivation {
-                inherit version;
-                name = "quick-yes-${version}";
-                src = ./quick-yes;
-                installPhase = ''
-                  mkdir -p $out/share/emacs/site-lisp
-                  cp *.el $out/share/emacs/site-lisp/
-                '';
-              };
-          sbt-mode = withPatches super.sbt-mode [
-            ./patches/sbt-mode/e9aa908d1b80dc2618eab22eeefc68ae82d0026f.patch
-          ];
-          seq = null; # https://github.com/NixOS/nixpkgs/issues/73346
-          title-capitalization =
-            let
-              version = "0.1";
-            in
-              stdenv.mkDerivation {
-                inherit version;
-                name = "title-capitalization-${version}";
-                src = fetchFromGitHub {
-                  owner = "novoid";
-                  repo = "title-capitalization.el";
-                  rev = "e83d463c500d04adf47b2e444728803121e7b641";
-                  sha256 = "0y0fhi8sb3chh5pzgn0rp7cy7492bw5yh1dldmpqxcsykjn06aga";
-                };
-                installPhase = ''
-                  mkdir -p $out/share/emacs/site-lisp
-                  cp *.el $out/share/emacs/site-lisp/
-                '';
-              };
-        };
+    package = doom-emacs;
   };
 }
